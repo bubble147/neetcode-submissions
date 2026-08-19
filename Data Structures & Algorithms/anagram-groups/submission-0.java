524644e825cd70c593f49baa787c9b0654998fa2
rarecandy <94965483+bubble147@users.noreply.github.com>
2026-08-19 12:31:14 +0900

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        java.util.Map<String, List<String>> map = new java.util.HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            java.util.Arrays.sort(chars);
+            String key = new String(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new java.util.ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new java.util.ArrayList<>(map.values());
+    }
+}
